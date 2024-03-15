812e435d54ee36deaebcfdf7ce792bf2e203779e
Tiago De Carvalho Estrada <124801828+TiagoDeCarvalhoEstrada@users.noreply.github.com>
2024-03-15 12:42:45 -0300

    Add files via upload

---
+++ projetos/ContaBancária/ContaBancária/Conta.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancária
+{
+    public class Conta
+    {
+        private string? Num_conta { get; set; }
+        private double Saldo { get; set; }
+        private double Limite { get; set; }
+
+        public void consulta_saldo()
+        {
+            Console.WriteLine("O seu saldo atual é de R$ " + Saldo);
+
+        }
+
+        public void depositar(double valor)
+        {
+            Saldo += valor;
+        }
+        public void sacar(double valor)
+        {
+            if (valor <= Saldo + Limite)
+            {
+                Saldo -= valor;
+            }
+            else
+            {
+                Console.WriteLine("Seu saldo é insuficiente :(");
+            }
+
+
+        }
+        public void ajuste_limite(double valor)
+        {
+            Limite = valor;
+        }
+    }
+}
---
+++ projetos/ContaBancária/ContaBancária/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ContaBancária
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-vindo ao banco ETEC MCM!");
+
+            //instanciar a classe Conta e criar um objeto dela
+            Conta conta = new Conta();
+            Console.WriteLine("CONSULTANDO O SALDO:");
+            conta.consulta_saldo();
+            Console.WriteLine("DEPÓSITO DE R$1000.35");
+            conta.depositar(1000.35);
+            Console.WriteLine("CONSULTANDO O SALDO:");
+            conta.consulta_saldo();
+
+            //Console.WriteLine("SAQUE DE R$500:");
+            //conta.sacar(500);
+            //Console.WriteLine("CONSULTANDO O SALDO:");
+            //conta.consulta_saldo();
+
+            //Console.WriteLine("SAQUE DE R$1000");
+            //conta.sacar(1000);
+
+            conta.ajuste_limite(1000);
+            conta.consulta_saldo();
+            conta.sacar(2000);
+            conta.consulta_saldo();
+        }
+    }
+}
