1e516ccd24433799f2faf519330ad9fa48032556
Tiago De Carvalho Estrada <124801828+TiagoDeCarvalhoEstrada@users.noreply.github.com>
2024-04-12 12:41:16 -0300

    Add files via upload

---
+++ Atividade2/Atividade2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Atividade2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoas p = new Pessoas();
+            p.Nome = "Tiago";
+            p.DataNasc = new DateTime(2007, 10, 14);
+            p.Altura = 1.76f;
+
+            p.ImprimirDados();
+
+            int idade = p.CalcularIdade();
+
+            Console.WriteLine("Idade: " + idade);
+
+            Console.ReadKey();
+        }
+    }
+}
