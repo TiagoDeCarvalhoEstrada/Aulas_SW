e454352f841042daa13b7f5168489f2260069022
Tiago De Carvalho Estrada <124801828+TiagoDeCarvalhoEstrada@users.noreply.github.com>
2024-05-16 19:39:52 -0300

    Add files via upload

---
+++ SW-Exercicios/Exercicio1/Exercicio9/Exercicio9/Livro.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio9
+{
+    public class Livro
+    {
+        private string _titulo; // Declaração de uma variável privada para armazenar o título do livro.
+        private string _autor; // Declaração de uma variável privada para armazenar o nome do autor do livro.
+
+        public Livro(string titulo, string autor) // Declaração do construtor da classe Livro, que recebe o título e o autor do livro como parâmetros.
+        {
+            ValidarDados(titulo, autor); // Chamada do método ValidarDados para verificar se os dados fornecidos são válidos.
+            _titulo = titulo; // Atribui o título fornecido ao campo privado _titulo.
+            _autor = autor; // Atribui o nome do autor fornecido ao campo privado _autor.
+        }
+
+        public string Titulo // Propriedade pública Titulo para acessar e modificar o título do livro.
+        {
+            get { return _titulo; } // Método getter que retorna o valor do título.
+            set { _titulo = value; } // Método setter que define o valor do título.
+        }
+
+        public string Autor // Propriedade pública Autor para acessar e modificar o nome do autor do livro.
+        {
+            get { return _autor; } // Método getter que retorna o valor do nome do autor.
+            set { _autor = value; } // Método setter que define o valor do nome do autor.
+        }
+
+        private void ValidarDados(string titulo, string nome) // Método privado para validar os dados do título e do autor do livro.
+        {
+            if (string.IsNullOrWhiteSpace(titulo)) // Verifica se o título é nulo, vazio ou contém apenas espaços em branco.
+            {
+                throw new ArgumentException("O título do livro não pode ser vazio ou nulo!"); // Lança uma exceção se o título for inválido.
+            }
+            else if (string.IsNullOrWhiteSpace(nome)) // Verifica se o nome do autor é nulo, vazio ou contém apenas espaços em branco.
+            {
+                throw new ArgumentException("O nome do autor não pode ser vazio ou nulo!"); // Lança uma exceção se o nome do autor for inválido.
+            }
+        }
+    }
+}
---
+++ SW-Exercicios/Exercicio1/Exercicio9/Exercicio9/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercicio9
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("******** Livro ********"); // Exibe uma mensagem no console indicando o início da interação com o programa.
+
+            Console.WriteLine("Digite os dados do Livro: "); // Solicita ao usuário que insira os dados do livro.
+            Console.Write("Título: "); // Solicita ao usuário que insira o título do livro.
+            string tituloDoLivro = Console.ReadLine(); // Lê o título do livro inserido pelo usuário e armazena na variável tituloDoLivro.
+            Console.Write("Autor: "); // Solicita ao usuário que insira o autor do livro.
+            string nomeDoAutor = Console.ReadLine(); // Lê o nome do autor inserido pelo usuário e armazena na variável nomeDoAutor.
+
+            Console.WriteLine("\n-------- Resultado --------"); // Exibe uma separação indicando o resultado.
+
+            try // Inicia um bloco try-catch para tratar possíveis exceções que podem ocorrer durante a execução do código.
+            {
+                Livro livro = new Livro(tituloDoLivro, nomeDoAutor); // Cria uma instância da classe Livro com o título e autor fornecidos pelo usuário.
+                Console.WriteLine($"Título: {livro.Titulo}"); // Exibe o título do livro na tela.
+                Console.WriteLine($"Autor: {livro.Autor}"); // Exibe o autor do livro na tela.
+            }
+            catch (ArgumentException exception) // Captura uma exceção do tipo ArgumentException, que pode ser lançada se os dados fornecidos forem inválidos.
+            {
+                Console.WriteLine($"Erro: {exception.Message}"); // Exibe a mensagem de erro associada à exceção capturada.
+            }
+
+            Console.ReadKey(); // Aguarda que o usuário pressione uma tecla antes de encerrar o programa.
+        }
+    }
+}
