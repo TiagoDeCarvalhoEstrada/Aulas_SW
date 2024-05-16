16577ade4c8ed2a1247fd33bb5d62ec3071d9b07
Tiago De Carvalho Estrada <124801828+TiagoDeCarvalhoEstrada@users.noreply.github.com>
2024-05-16 19:42:15 -0300

    Add files via upload

---
+++ SW-Exercicios/Exercicio1/Exercicio10/Exercicio10/Animal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio10
+{
+    public class Animal // Declaração da classe Animal.
+    {
+        private string _nome; // Declaração de uma variável privada para armazenar o nome do animal.
+        private string _especie; // Declaração de uma variável privada para armazenar a espécie do animal.
+        private int _idade; // Declaração de uma variável privada para armazenar a idade do animal.
+
+        public Animal(string nome, string especie, int idade) // Construtor da classe Animal que recebe o nome, espécie e idade como parâmetros.
+        {
+            _nome = nome; // Inicializa a variável _nome com o valor do parâmetro nome.
+            _especie = especie; // Inicializa a variável _especie com o valor do parâmetro especie.
+            _idade = idade; // Inicializa a variável _idade com o valor do parâmetro idade.
+        }
+
+        public string Nome // Propriedade pública Nome que permite acessar e modificar o nome do animal.
+        {
+            get { return _nome; } // Retorna o valor armazenado na variável _nome.
+            set { _nome = value; } // Define o valor da variável _nome com o valor passado como parâmetro.
+        }
+
+        public string Especie // Propriedade pública Especie que permite acessar e modificar a espécie do animal.
+        {
+            get { return _especie; } // Retorna o valor armazenado na variável _especie.
+            set { _especie = value; } // Define o valor da variável _especie com o valor passado como parâmetro.
+        }
+
+        public int Idade // Propriedade pública Idade que permite acessar e modificar a idade do animal.
+        {
+            get { return _idade; } // Retorna o valor armazenado na variável _idade.
+            set { _idade = value; } // Define o valor da variável _idade com o valor passado como parâmetro.
+        }
+
+        public void EmitirSom(string somDoAnimal) // Método público EmitirSom que exibe o som emitido pelo animal.
+        {
+            Console.WriteLine(somDoAnimal); // Exibe o som emitido pelo animal no console.
+        }
+    }
+}
---
+++ SW-Exercicios/Exercicio1/Exercicio10/Exercicio10/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercicio10
+{
+    internal class Program // Declaração de uma classe interna chamada Program.
+    {
+        private static void Main(string[] args) // Método de entrada do programa.
+        {
+            Console.WriteLine("***** Animal *****"); // Exibe uma mensagem no console indicando o início da interação com o programa.
+
+            Console.WriteLine("\nDigite as informações do Animal: "); // Solicita ao usuário que insira as informações do animal.
+            Console.Write("Nome: "); // Solicita ao usuário que insira o nome do animal.
+            string nomeDoAnimal = Console.ReadLine(); // Lê o nome do animal inserido pelo usuário e armazena na variável nomeDoAnimal.
+            Console.Write("Espécie: "); // Solicita ao usuário que insira a espécie do animal.
+            string especieDoAnimal = Console.ReadLine(); // Lê a espécie do animal inserida pelo usuário e armazena na variável especieDoAnimal.
+            Console.Write("Idade: "); // Solicita ao usuário que insira a idade do animal.
+            int idadeDoAnimal = int.Parse(Console.ReadLine()); // Lê a idade do animal inserida pelo usuário e converte para um inteiro, armazenando na variável idadeDoAnimal.
+            Console.Write("Som: "); // Solicita ao usuário que insira o som emitido pelo animal.
+            string somDoAnimal = Console.ReadLine(); // Lê o som emitido pelo animal inserido pelo usuário e armazena na variável somDoAnimal.
+
+            Animal animal = new Animal(nomeDoAnimal, especieDoAnimal, idadeDoAnimal); // Cria uma instância da classe Animal com as informações fornecidas pelo usuário.
+
+            Console.WriteLine("\n----- Resultado -----"); // Exibe uma separação indicando o resultado.
+            Console.WriteLine($"Nome: {animal.Nome}"); // Exibe o nome do animal na tela.
+            Console.WriteLine($"Espécie: {animal.Especie}"); // Exibe a espécie do animal na tela.
+            Console.WriteLine($"Idade: {animal.Idade} anos"); // Exibe a idade do animal na tela.
+            Console.Write($"O animal emite o som: ");
+            animal.EmitirSom(somDoAnimal); // Chama o método EmitirSom da instância animal, passando o som emitido pelo animal como argumento.
+
+            Console.ReadKey(); // Aguarda que o usuário pressione uma tecla antes de encerrar o programa.
+        }
+    }
+}
