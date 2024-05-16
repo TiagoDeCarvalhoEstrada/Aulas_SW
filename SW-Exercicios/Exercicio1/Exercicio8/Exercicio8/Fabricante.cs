5c46c25d495f6dcef094e6df453d56aed07df624
Tiago De Carvalho Estrada <124801828+TiagoDeCarvalhoEstrada@users.noreply.github.com>
2024-05-16 19:33:33 -0300

    Add files via upload

---
+++ SW-Exercicios/Exercicio1/Exercicio8/Exercicio8/Fabricante.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio8
+{
+    public class Fabricante
+    {
+        // Propriedades da classe Fabricante
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Cidade { get; set; }
+
+        // Construtor da classe Fabricante
+        public Fabricante(string nome, string endereco, string cidade)
+        {
+            Nome = nome;
+            Endereco = endereco;
+            Cidade = cidade;
+        }
+    }
+}
